e3340d4ce90ee27484cfea8adeae42f745cdf734
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-04 17:05:26 +0900

    🔧 export env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+require('dotenv').config({ path: './.env.local' })
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    ENDPOINT: process.env.ENDPOINT,
+    PROFILE_API_KEY: process.env.PROFILE_API_KEY,
+  },
 }
