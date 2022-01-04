81a07d37aa0d79a26d29cd49dcb0f0013bcf3385
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-04 17:10:44 +0900

    🏷️ update type

--- src/libs/microCMS/getProfile.ts
+++ src/libs/microCMS/getProfile.ts
@@ -5,14 +5,17 @@ import { profile } from '../../types/cms-types'
 const PROFILE_API_KEY: string = process.env.PROFILE_API_KEY || ''
 const ENDPOINT: string = process.env.ENDPOINT || ''
 
-export const getProfile = async (): Promise<AxiosResponse<any, any>> => {
+export const getProfile = async (): Promise<AxiosResponse<profile, any>> => {
   try {
-    const res: AxiosResponse<profile> = await axios.get(`${ENDPOINT}/profile`, {
-      headers: {
-        'Content-type': 'application/json',
-        'X-MICROCMS-API-KEY': PROFILE_API_KEY,
-      },
-    })
+    const res: AxiosResponse<profile, any> = await axios.get(
+      `${ENDPOINT}/profile`,
+      {
+        headers: {
+          'Content-type': 'application/json',
+          'X-MICROCMS-API-KEY': PROFILE_API_KEY,
+        },
+      }
+    )
     return res
   } catch (e) {
     console.error(e)
