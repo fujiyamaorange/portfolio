bcf9e631a7ec08af0cd45e41568a9853aedcc8f8
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-04 17:03:12 +0900

    ✨ add get profile function

---
+++ src/libs/microCMS/getProfile.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosResponse } from 'axios'
+
+import { profile } from '../../types/cms-types'
+
+const PROFILE_API_KEY: string = process.env.PROFILE_API_KEY || ''
+const ENDPOINT: string = process.env.ENDPOINT || ''
+
+export const getProfile = async (): Promise<AxiosResponse<any, any>> => {
+  try {
+    const res: AxiosResponse<profile> = await axios.get(`${ENDPOINT}/profile`, {
+      headers: {
+        'Content-type': 'application/json',
+        'X-MICROCMS-API-KEY': PROFILE_API_KEY,
+      },
+    })
+    return res
+  } catch (e) {
+    console.error(e)
+    throw e
+  }
+}
