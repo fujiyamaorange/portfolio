4aa7ca91f545018add258effa7db5284c227dbbd
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-05 14:27:35 +0900

    ✏️ export

--- src/components/basic/Div100vh.tsx
+++ src/components/basic/Div100vh.tsx
@@ -2,16 +2,17 @@ import { forwardRef, HTMLAttributes } from 'react'
 
 import { use100vh } from '@/hooks/use100vh'
 
-const Div100vh = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
-  ({ style, ...other }, ref) => {
-    const height = use100vh()
+export const Div100vh = forwardRef<
+  HTMLDivElement,
+  HTMLAttributes<HTMLDivElement>
+>(({ style, ...other }, ref) => {
+  const height = use100vh()
 
-    const styleWithRealHeight = {
-      ...style,
-      height: height ? `${height}px` : '100vh',
-    }
-    return <div ref={ref} style={styleWithRealHeight} {...other} />
+  const styleWithRealHeight = {
+    ...style,
+    height: height ? `${height}px` : '100vh',
   }
-)
+  return <div ref={ref} style={styleWithRealHeight} {...other} />
+})
 
 Div100vh.displayName = 'Div100vh'
