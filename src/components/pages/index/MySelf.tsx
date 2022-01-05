b77edaaaa94ad6125a2bc583c46ae8a0cb7c5ab2
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-05 15:28:48 +0900

    💫 add typed.js animation

--- src/components/pages/index/MySelf.tsx
+++ src/components/pages/index/MySelf.tsx
@@ -1,22 +1,47 @@
+import { useEffect, useRef } from 'react'
 import Image from 'next/image'
-import Head from 'next/head'
+import Typed from 'typed.js'
 
 import { profile } from '@/types/cms-types'
+import { Div100vh } from '@/components/basic/Div100vh'
 
 type Props = { data: profile }
 
 export const MySelf: React.VFC<Props> = (props) => {
   const { name, introduce, image, hobby } = props.data
 
-  return (
-    <>
-      <Head>
-        <title>FujiyamaOrange Portfolio</title>
-        <meta name="description" content="Fujimura Kaito Portfilio" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  // Create reference to store the DOM element containing the animation
+  const el = useRef(null!)
+  // Create reference to store the Typed instance itself
+  const typed = useRef<Typed>(null!)
 
-      <main>
+  useEffect(() => {
+    const options = {
+      strings: [
+        '<span>Hello&sbquo;&ensp;</span>I&rsquo;m Fujimura Kaito.',
+        'I live in Fukuoka&sbquo; Japan.',
+        'I&rsquo;m a university student.',
+      ],
+      typeSpeed: 100,
+      backSpeed: 30,
+    }
+
+    // elRef refers to the <span> rendered below
+    typed.current = new Typed(el.current, options)
+
+    return () => {
+      // Make sure to destroy Typed instance during cleanup
+      // to prevent memory leaks
+      typed.current.destroy()
+    }
+  }, [])
+
+  return (
+    <main className="bg-black text-white">
+      <Div100vh className="flex relative items-center justify-center select-none">
+        <span className="font-semibold text-2xl" ref={el} />
+      </Div100vh>
+      <section>
         {props && (
           <>
             <h3>名前</h3>
@@ -29,7 +54,8 @@ export const MySelf: React.VFC<Props> = (props) => {
             <p>{hobby}</p>
           </>
         )}
-      </main>
-    </>
+        <h1>残りの要素</h1>
+      </section>
+    </main>
   )
 }
