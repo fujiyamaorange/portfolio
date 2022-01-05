b70ccf6dd87f8c0f79de5e3cf04030689f9d4100
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-05 16:45:40 +0900

    ✨ add ShootingStars

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,9 +1,11 @@
 import { Nav } from './Nav'
+import { ShootingStarts } from '../anime/ShootingStars'
 
 export const Layout: React.FC = ({ children }) => {
   return (
     <>
       {/* ヘッダーと背景のアニメーション */}
+      <ShootingStarts />
       <Nav />
       {children}
     </>
