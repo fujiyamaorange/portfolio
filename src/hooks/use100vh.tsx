9b1acf8a37f78c25fe1df3c29b29d3c36ad1ec4a
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-05 14:26:37 +0900

    🚸 add 100vh

---
+++ src/hooks/use100vh.tsx
@@ -0,0 +1,50 @@
+// https://github.com/mvasin/react-div-100vh/blob/master/packages/react-div-100vh/src/index.tsx
+
+import { useState, useEffect } from 'react'
+import { isMobile } from 'react-device-detect'
+
+export const use100vh = (): number | null => {
+  const [height, setHeight] = useState<number | null>(measureHeight)
+
+  const wasRenderedOnClientAtLeastOnce = useWasRenderedOnClientAtLeastOnce()
+
+  useEffect(() => {
+    if (!wasRenderedOnClientAtLeastOnce) return
+
+    const setMeasuredHeight = () => {
+      const measuredHeight = measureHeight()
+      setHeight(measuredHeight)
+    }
+
+    if (!isMobile) {
+      window.addEventListener('resize', setMeasuredHeight)
+    }
+
+    return () => window.removeEventListener('resize', setMeasuredHeight)
+  }, [wasRenderedOnClientAtLeastOnce])
+  return wasRenderedOnClientAtLeastOnce ? height : null
+}
+
+export const measureHeight = (): number | null => {
+  if (!isClient()) return null
+  return window.innerHeight
+}
+
+// It's not needed for CSR-only apps, but is critical for SSR.
+const useWasRenderedOnClientAtLeastOnce = () => {
+  const [
+    wasRenderedOnClientAtLeastOnce,
+    setWasRenderedOnClientAtLeastOnce,
+  ] = useState(false)
+
+  useEffect(() => {
+    if (isClient()) {
+      setWasRenderedOnClientAtLeastOnce(true)
+    }
+  }, [])
+  return wasRenderedOnClientAtLeastOnce
+}
+
+const isClient = () => {
+  return typeof window !== 'undefined' && typeof document !== 'undefined'
+}
