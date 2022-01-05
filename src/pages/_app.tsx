e0fd6cbe5c549f4202a3c1d2ba41619c7cc11d59
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-05 15:28:24 +0900

    ✨ use layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/globals.css'
+import { Layout } from '@/components/layout'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>FujiyamaOrange Portfolio</title>
+        <meta name="description" content="Fujimura Kaito Portfilio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
-
-export default MyApp
