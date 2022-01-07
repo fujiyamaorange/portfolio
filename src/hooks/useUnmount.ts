c41f96ebd49218a2220ca22f2a049ff69ad5faea
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-07 09:18:02 +0900

    ✨ add unmount hook

---
+++ src/hooks/useUnmount.ts
@@ -0,0 +1,11 @@
+import { useRef, useEffect } from 'react'
+
+export const useUnmount = (fn: () => any): void => {
+  const fnRef = useRef(fn)
+
+  fnRef.current = fn
+
+  useEffect(() => {
+    return () => fnRef.current()
+  }, [])
+}
