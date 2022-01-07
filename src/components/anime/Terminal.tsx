2ec51f5cd2171b6066dfc87f5ddcd3446edfeca9
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-07 17:54:09 +0900

    💫 add terminal window animation

---
+++ src/components/anime/Terminal.tsx
@@ -0,0 +1,51 @@
+import clsx from 'clsx'
+import styles from '@/styles/Terminal.module.css'
+import { profile } from '@/types/cms-types'
+
+type Props = {
+  data: profile
+}
+
+export const Terminal: React.VFC<Props> = ({ data }) => {
+  const { name, introduce, hobby } = data
+
+  return (
+    <main className={styles.container}>
+      <div className={styles.terminal}>
+        <section className={styles.terminal__bar}>
+          <div className={styles.bar__buttons}>
+            <button
+              className={clsx(styles.bar__button, styles.bar__button__exit)}
+            >
+              &#10005;
+            </button>
+            <button className={styles.bar__button}>&#9472;</button>
+            <button className={styles.bar__button}>&#9723;</button>
+          </div>
+          <p className={styles.bar__user}>user@ubuntu: ~</p>
+        </section>
+        <section className={styles.terminal__body}>
+          <div className={styles.terminal__prompt}>
+            <span className={styles.terminal__prompt__user}>user@ubuntu:</span>
+            <span className={styles.terminal__prompt__location}>~</span>
+            <span className={styles.terminal__prompt__bling}>$</span>
+            <p>{name}</p>
+          </div>
+          <div className={styles.terminal__prompt}>
+            <span className={styles.terminal__prompt__user}>user@ubuntu:</span>
+            <span className={styles.terminal__prompt__location}>~</span>
+            <span className={styles.terminal__prompt__bling}>$</span>
+            <div dangerouslySetInnerHTML={{ __html: introduce }}></div>
+          </div>
+          <div className={styles.terminal__prompt}>
+            <span className={styles.terminal__prompt__user}>user@ubuntu:</span>
+            <span className={styles.terminal__prompt__location}>~</span>
+            <span className={styles.terminal__prompt__bling}>$</span>
+            <p>{hobby}</p>
+            <span className={styles.terminal__prompt__cursor}></span>
+          </div>
+        </section>
+      </div>
+    </main>
+  )
+}
