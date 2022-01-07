fc909718890a23d52fd46502a64f2b873a707a07
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-07 17:28:38 +0900

    💄 update nav style

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -17,11 +17,13 @@ export const Nav: React.VFC = () => {
   return (
     <header className="flex fixed z-50 bg-opacity-80 w-full justify-between items-center h-16 bg-black text-white font-semibold px-4 sm:px-8">
       <h1>Portfolio</h1>
-      <ul className="flex space-x-4">
+      <ul className="flex">
         {menus.map((menu) => (
-          <button key={menu.name}>
-            <li>{menu.name}</li>
-          </button>
+          <li key={menu.name}>
+            <button className="hover:text-gray-300 transition duration-150 px-2 py-2">
+              {menu.name}
+            </button>
+          </li>
         ))}
       </ul>
     </header>
