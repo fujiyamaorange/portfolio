bcc6b5fb40a9a5289ead5a6f60cf8f4edbd0bcc3
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-08 16:00:16 +0900

    ✨ add on and off utils

---
+++ src/utils/off.ts
@@ -0,0 +1,12 @@
+export function off<T extends Window | Document | HTMLElement | EventTarget>(
+  obj: T | null,
+  ...args:
+    | Parameters<T['removeEventListener']>
+    | [string, Function | null, ...any[]]
+): void {
+  if (obj && obj.removeEventListener) {
+    obj.removeEventListener(
+      ...(args as Parameters<HTMLElement['removeEventListener']>)
+    )
+  }
+}
