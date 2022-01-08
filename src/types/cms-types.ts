8a514f1feb5ac44e40bab7e288ff94511b466098
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-08 16:41:29 +0900

    🏷️ add image type to works schema

--- src/types/cms-types.ts
+++ src/types/cms-types.ts
@@ -44,6 +44,10 @@ T,
    * 使用技術
    */
   skill?: ('TypeScript' | 'JavaScript' | 'Python' | 'C' | 'Java' | 'C++' | 'React' | 'Vue.js' | 'Svelte' | 'Angular' | 'Next.js' | 'Nuxt.js' | 'Dart' | 'Flutter' | 'Figma' | 'Github' | 'Firebase Firestore' | 'Firebase Cloud Storage' | 'Firebase Cloud Function' | 'Firebase Authentication' | 'GCP' | 'AWS' | 'TailwindCSS' | 'Node.js' | 'HTML' | 'CSS' | 'styled-jsx' | 'microCMS' | 'Express' | 'NestJS' | 'Apollo' | 'GraphQL' | 'Heroku' | 'Vercel' | 'Redux' | 'Recoil')[]
+  /**
+   * 画像
+   */
+  image?: { url: string, width: number, height: number }
 }>
 
 export type profile<T='get'> = Structure<
