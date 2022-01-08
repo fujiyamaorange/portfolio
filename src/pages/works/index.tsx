899ce93d1ffb382b5462bca7fd06bc3db21c3bea
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-08 17:18:32 +0900

    ✨ add get works function

--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,18 +1,28 @@
-import { NextPage, GetStaticPropsContext } from 'next'
+import { NextPage, GetStaticPropsContext, InferGetStaticPropsType } from 'next'
 
 import { WorksList } from '@/components/pages/works/WorksList'
+import { getWorks } from '@/libs/microCMS/getWorks'
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>
 
 export const getStaticProps = async (context: GetStaticPropsContext) => {
-  // get data from microCMS
+  const res = await getWorks()
+  if (res.status === 200) {
+    console.log('🍊')
+  } else {
+    // エラー
+    console.log('💣')
+  }
+
   return {
     props: {
-      data: {},
+      data: res.data.contents,
     },
   }
 }
 
-const Works: NextPage = () => {
-  return <WorksList />
+const Works: NextPage<Props> = (props) => {
+  return <WorksList data={props.data} />
 }
 
 export default Works
