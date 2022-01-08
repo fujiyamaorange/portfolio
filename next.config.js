ddf5fc537c491b62c3187867a3c06c41f2432d4e
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-08 17:17:32 +0900

    🔧 add env variant

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ module.exports = {
   env: {
     ENDPOINT: process.env.ENDPOINT,
     PROFILE_API_KEY: process.env.PROFILE_API_KEY,
+    WORKS_API_KEY: process.env.WORKS_API_KEY,
   },
   images: {
     domains: ['images.microcms-assets.io'],
