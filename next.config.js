54c8c19c7e7b80384e2591224b044f7a8d15b6d6
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-09 23:32:47 +0900

    🔥 remove config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ require('dotenv').config({ path: './.env.local' })
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  optimizeFonts: true,
   env: {
     ENDPOINT: process.env.ENDPOINT,
     PROFILE_API_KEY: process.env.PROFILE_API_KEY,
