286338bcc8a530866bb01b261c178fe30c1be031
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-09 23:01:35 +0900

    🔧 update confit for font optimization

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ require('dotenv').config({ path: './.env.local' })
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  optimizeFonts: true,
   env: {
     ENDPOINT: process.env.ENDPOINT,
     PROFILE_API_KEY: process.env.PROFILE_API_KEY,
