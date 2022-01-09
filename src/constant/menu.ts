623f332e463729fa09048be32386f551fda361f4
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-09 14:12:56 +0900

    🤡 add menu constant

---
+++ src/constant/menu.ts
@@ -0,0 +1,19 @@
+export type MENU = {
+  name: string
+  url: string
+}
+
+export const menus: MENU[] = [
+  {
+    name: 'Home',
+    url: '/',
+  },
+  {
+    name: 'Works',
+    url: '/works',
+  },
+  {
+    name: 'Contact',
+    url: '/contact',
+  },
+]
