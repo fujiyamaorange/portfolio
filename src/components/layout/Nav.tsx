c3033a1ea4ac85927511b492e806be0ead31c952
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-12 10:38:28 +0900

    ♿️ update a11y

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -19,7 +19,11 @@ export const Nav: React.VFC = () => {
     <header className="fixed z-50 flex items-center justify-between w-full h-16 px-4 font-semibold text-white bg-black bg-opacity-80 sm:px-8">
       <div className="flex items-center space-x-4">
         {/* https://github.com/vercel/next.js/issues/18585 */}
-        <a target="_blank" href="https://github.com/FujimuraKaito">
+        <a
+          target="_blank"
+          href="https://github.com/FujimuraKaito"
+          className="rounded-md focus:outline-none focus:ring-2 focus:ring-white"
+        >
           <NextImage
             src="/GitHub64pxWhite.png"
             width={32}
@@ -28,7 +32,11 @@ export const Nav: React.VFC = () => {
             className="w-8 h-8 hover:animate-spin"
           />
         </a>
-        <a target="_blank" href="https://zenn.dev/fujiyama">
+        <a
+          target="_blank"
+          href="https://zenn.dev/fujiyama"
+          className="rounded-md focus:outline-none focus:ring-2 focus:ring-white"
+        >
           <NextImage
             src="/zennwhite.svg"
             width={32}
@@ -43,7 +51,7 @@ export const Nav: React.VFC = () => {
           <li key={menu.name}>
             <button
               onClick={(e) => handleClick(e, menu.url)}
-              className="px-2 py-2 transition duration-300 hover:text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-blue-500"
+              className="px-2 py-2 transition duration-300 rounded-md hover:text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-blue-500 focus:outline-none focus:ring-2 focus:ring-white"
             >
               {menu.name}
             </button>
