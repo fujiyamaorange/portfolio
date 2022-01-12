e593e2858c9a7254f78db87b9d9e39587a66d0e9
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-12 10:24:17 +0900

    💫 add slidein animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,38 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    keyframes: {
+      'slidein-from-left': {
+        '0%': {
+          transform: 'translateX(-150%)',
+        },
+        '100%': {
+          transform: 'translateX(0)',
+        },
+      },
+
+      'slidein-from-right': {
+        '0%': {
+          transform: 'translateX(150%)',
+        },
+        '100%': {
+          transform: 'translateX(0)',
+        },
+      },
+      spin: {
+        '0%': {
+          transform: 'rotate(0deg)',
+        },
+        '100%': {
+          transform: 'rotate(360deg)',
+        },
+      },
+    },
+    animation: {
+      'slidein-from-left': 'slidein-from-left 0.8s ease-out',
+      'slidein-from-right': 'slidein-from-right 0.8s ease-out',
+      spin: 'spin 1s linear infinite',
+    },
   },
   plugins: [],
 }
