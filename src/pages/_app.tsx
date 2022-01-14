594affbb6cf31b6ae4d9713703b6f598fe6680d2
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-14 09:20:07 +0900

    ♻️ add canonical link

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -52,6 +52,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           cardType: 'summary',
         }}
       />
+      <Head>
+        <link rel="canonical" href="https://fujiyamaorange.vercel.app" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
