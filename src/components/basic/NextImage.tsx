1c76162ed48a24a18cc468441e735f1f3de93671
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-19 17:01:43 +0900

    ♻️ add alt tag

--- src/components/basic/NextImage.tsx
+++ src/components/basic/NextImage.tsx
@@ -1,9 +1,13 @@
 import Image, { ImageProps } from 'next/image'
 
-export const NextImage: React.VFC<ImageProps> = ({ className, ...others }) => {
+export const NextImage: React.VFC<ImageProps> = ({
+  className,
+  alt,
+  ...others
+}) => {
   return (
     <div className={className}>
-      <Image {...others} />
+      <Image {...others} alt={alt} />
     </div>
   )
 }
