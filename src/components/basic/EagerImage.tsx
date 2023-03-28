967c2a92c5fcb62ee72b109e21f145ee1ba9571a
fujiyamaorange <kaito.bakuman@gmail.com>
2023-03-28 16:06:09 +0900

    create image component which load src if this component exists in viewport

---
+++ src/components/basic/EagerImage.tsx
@@ -0,0 +1,47 @@
+import { DetailedHTMLProps, ImgHTMLAttributes, useEffect, useRef } from 'react'
+
+export const EagerImage = ({
+  className,
+  src,
+  alt,
+  ...others
+}: DetailedHTMLProps<
+  ImgHTMLAttributes<HTMLImageElement>,
+  HTMLImageElement
+>) => {
+  const imgRef = useRef<HTMLImageElement>(null)
+
+  useEffect(() => {
+    if (!imgRef.current) return
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && imgRef.current) {
+            imgRef.current.src = src ?? ''
+            observer.unobserve(entry.target)
+          }
+        })
+      },
+      {
+        rootMargin: '0px',
+        threshold: 0.5,
+      }
+    )
+
+    observer.observe(imgRef.current)
+
+    return () => {
+      if (imgRef.current) {
+        observer.unobserve(imgRef.current)
+      }
+    }
+  }, [])
+
+  return (
+    <div className={className}>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img {...others} ref={imgRef} data-src={src} alt={alt} />
+    </div>
+  )
+}
