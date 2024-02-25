e0037d4a8af5a97708c4e0b4d9bc51f271440871
fujiyamaorange <kaito.bakuman@gmail.com>
2024-02-25 16:31:14 +0900

    fix: fix text size

--- src/components/anime/CameraContent.tsx
+++ src/components/anime/CameraContent.tsx
@@ -1,17 +1,17 @@
 import { profile } from "@/types/cms-types";
 
 type Props = {
-  data: profile;
+	data: profile;
 };
 
 export const CameraContent = ({ data }: Props) => {
-  const { name, introduce, hobby } = data;
-  return (
-    <div className="mx-auto my-4 space-y-4 text-xl text-center w-max">
-      <h2>{name}</h2>
-      {/* biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation> */}
-      <div dangerouslySetInnerHTML={{ __html: introduce }} />
-      <h2>{hobby}</h2>
-    </div>
-  );
+	const { name, introduce, hobby } = data;
+	return (
+		<div className="mx-auto my-4 space-y-4 text-lg text-center w-max">
+			<h2>{name}</h2>
+			{/* biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation> */}
+			<div dangerouslySetInnerHTML={{ __html: introduce }} />
+			<h2>{hobby}</h2>
+		</div>
+	);
 };
--- src/components/pages/index/MySelf.tsx
+++ src/components/pages/index/MySelf.tsx
@@ -11,61 +11,61 @@ import { profile } from "@/types/cms-types";
 type Props = { data: profile };
 
 export const MySelf = (props: Props) => {
-  const { name, image } = props.data;
+	const { name, image } = props.data;
 
-  const isUnder540 = useMedia("(max-width: 540px)");
+	const isUnder540 = useMedia("(max-width: 540px)");
 
-  const el = useRef(null);
+	const el = useRef(null);
 
-  useEffect(() => {
-    const options = {
-      strings: [
-        "<span>Hello&sbquo;&ensp;</span>I&rsquo;m Fujimura Kaito.",
-        "I live in Fukuoka&sbquo; Japan.",
-        "I&rsquo;m a Web Developer.",
-      ],
-      typeSpeed: 100,
-      backSpeed: 30,
-      loop: true,
-    };
+	useEffect(() => {
+		const options = {
+			strings: [
+				"<span>Hello&sbquo;&ensp;</span>I&rsquo;m Fujimura Kaito.",
+				"I live in Fukuoka&sbquo; Japan.",
+				"I&rsquo;m a Web Developer.",
+			],
+			typeSpeed: 100,
+			backSpeed: 30,
+			loop: true,
+		};
 
-    const typed = new Typed(el.current ?? "", options);
+		const typed = new Typed(el.current ?? "", options);
 
-    return () => {
-      typed.destroy();
-    };
-  }, []);
+		return () => {
+			typed.destroy();
+		};
+	}, []);
 
-  return (
-    <main className="text-white">
-      <Div100vh className="relative flex items-center justify-center select-none">
-        <span
-          className="text-2xl font-semibold text-transparent sm:text-4xl bg-clip-text bg-gradient-to-r from-pink-600 to-blue-500"
-          ref={el}
-        />
-      </Div100vh>
-      <section className="flex flex-col items-center justify-center selection:bg-transparent">
-        <Image
-          src={image.url}
-          width={image.width}
-          height={image.height}
-          className="rounded-full"
-          aria-hidden="true"
-          alt="user icon"
-        />
-        <span className="mt-4 font-semibold selection:bg-white selection:text-black">
-          {name}
-        </span>
-      </section>
-      <section>
-        {isUnder540 ? (
-          <div className="mt-16 mb-32">
-            <CameraContent data={props.data} />
-          </div>
-        ) : (
-          <Terminal data={props.data} />
-        )}
-      </section>
-    </main>
-  );
+	return (
+		<main className="text-white">
+			<Div100vh className="relative flex items-center justify-center select-none">
+				<span
+					className="text-2xl font-semibold text-transparent sm:text-4xl bg-clip-text bg-gradient-to-r from-pink-600 to-blue-500"
+					ref={el}
+				/>
+			</Div100vh>
+			<section className="flex flex-col items-center justify-center selection:bg-transparent">
+				<Image
+					src={image.url}
+					width={image.width}
+					height={image.height}
+					className="rounded-full"
+					aria-hidden="true"
+					alt="user icon"
+				/>
+				<span className="mt-4 text-2xl font-semibold selection:bg-white selection:text-black">
+					{name}
+				</span>
+			</section>
+			<section>
+				{isUnder540 ? (
+					<div className="mt-16 mb-32">
+						<CameraContent data={props.data} />
+					</div>
+				) : (
+					<Terminal data={props.data} />
+				)}
+			</section>
+		</main>
+	);
 };
